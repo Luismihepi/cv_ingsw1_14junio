e7f7da2c087247c365f97186afea44c5f915c842
Usuario <Usuario@Usuario-PC>
2022-06-14 17:27:38 -0500

    version 1 prueba

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -15,6 +15,7 @@ public class Cv_ejercicio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("version 1");
         // TODO code application logic here
     }
     
